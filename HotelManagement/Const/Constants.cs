be86e6cdc532bfb948b5b35bafde3192fbb974e6
shiyuetc <shiyuetc@users.noreply.github.com>
2020-10-31 09:00:58 +0900

    定数クラスの追加

---
+++ HotelManagement/Const/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Const
+{
+    public static class Constants
+    {
+        public static char[] WeekNames = { '日', '月', '火', '水', '木', '金', '土' };
+    }
+}
