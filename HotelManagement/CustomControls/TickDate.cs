056840b64139d7b40ab57c16fb511a6e0034ebc2
shiyuetc <shiyuetc@users.noreply.github.com>
2020-11-02 10:02:11 +0900

    TickDateにコメントを追記

--- HotelManagement/CustomControls/TickDate.cs
+++ HotelManagement/CustomControls/TickDate.cs
@@ -4,19 +4,36 @@ using System.Windows.Forms;
 
 namespace HotelManagement.CustomControls
 {
+    /// <summary>
+    /// 現在時刻を表示するコントロール
+    /// </summary>
     public partial class TickDate : UserControl
     {
+        /// <summary>
+        /// TickDateを初期化します。
+        /// </summary>
         public TickDate()
         {
             InitializeComponent();
+
+            // 時刻を更新
             this.UpdateDateTime(DateTime.Now);
         }
 
+        /// <summary>
+        /// TickTimerのTickイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TickTimer_Tick(object sender, EventArgs e)
         {
             this.UpdateDateTime(DateTime.Now);
         }
-
+        
+        /// <summary>
+        /// 表示時刻を更新します。
+        /// </summary>
+        /// <param name="nowDateTime">更新する時刻</param>
         private void UpdateDateTime(DateTime nowDateTime)
         {
             this.DateLabel.Text = nowDateTime.ToString("yyyy年MM月dd日");
