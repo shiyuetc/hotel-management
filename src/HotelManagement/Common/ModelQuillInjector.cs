2fa297cd4e502e69ce556c0d754ff7b3f4bbcbac
shiyuetc <shiyuetc@users.noreply.github.com>
2020-11-02 23:25:02 +0900

    ModelQuillInjectorを追加

---
+++ src/HotelManagement/Common/ModelQuillInjector.cs
@@ -0,0 +1,14 @@
+﻿using Seasar.Quill;
+
+namespace HotelManagement.Common
+{
+    public class ModelQuillInjector<T>
+    {
+        readonly public T Model;
+
+        public ModelQuillInjector()
+        {
+            QuillInjector.GetInstance().Inject(this);
+        }
+    }
+}
