55bd5ede09549a4b6a9212a874d0b0a46c248821
shiyuetc <shiyuetc@users.noreply.github.com>
2020-11-02 10:51:25 +0900

    Constantsにコメントを追記

--- HotelManagement/Const/Constants.cs
+++ HotelManagement/Const/Constants.cs
@@ -1,8 +1,14 @@
 ﻿
 namespace HotelManagement.Const
 {
+    /// <summary>
+    /// 定数
+    /// </summary>
     public static class Constants
     {
+        /// <summary>
+        /// DayOfWeek列挙体に対応した日本語
+        /// </summary>
         public static char[] WeekNames = { '日', '月', '火', '水', '木', '金', '土' };
     }
 }
