06cda0a5871318c01b140a3d43fb33388a8272f9
shiyuetc <shiyuetc@users.noreply.github.com>
2020-11-02 10:02:54 +0900

    不要なusingを削除

--- HotelManagement/Const/Constants.cs
+++ HotelManagement/Const/Constants.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace HotelManagement.Const
 {
     public static class Constants
