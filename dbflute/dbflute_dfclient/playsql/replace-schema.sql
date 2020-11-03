090e70ea79388810f3d9ce0c3aea8624d1516bc7
shiyuetc <shiyuetc@users.noreply.github.com>
2020-11-03 13:36:20 +0900

    DB更新

--- dbflute/dbflute_dfclient/playsql/replace-schema.sql
+++ dbflute/dbflute_dfclient/playsql/replace-schema.sql
@@ -1,22 +1,24 @@
 -- KbnRank
 CREATE TABLE "kbn_rank" (
     "code" VARCHAR(3) NOT NULL PRIMARY KEY,
-    "name" VARCHAR(20) NOT NULL
-)
+    "english_name" VARCHAR(20) NOT NULL,
+    "japanese_name" VARCHAR(20) NOT NULL
+);
 
 -- MstEmployee
 CREATE TABLE "mst_employee" (
     "id" SERIAL NOT NULL PRIMARY KEY,
-    "employee_no" VARCHAR(20) UNIQUE NOT NULL,
+    "employee_no" VARCHAR(10) UNIQUE NOT NULL,
     "name" VARCHAR(50) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
     "rank_code" VARCHAR(3) NOT NULL,
-    "join_date_time" TIMESTAMP(3) NOT NULL,
-    "leave_date_time" TIMESTAMP(3) NOT NULL
-)
+    "entry_date" TIMESTAMP(3) NOT NULL,
+    "leave_date" TIMESTAMP(3),
+    FOREIGN KEY ("rank_code") REFERENCES "kbn_rank"("code")
+);
 
 -- MstCustomer
 CREATE TABLE "mst_customer" (
     "id" SERIAL NOT NULL PRIMARY KEY,
-    "name" VARCHAR(50) NOT NULL,
-)
+    "name" VARCHAR(50) NOT NULL
+);
