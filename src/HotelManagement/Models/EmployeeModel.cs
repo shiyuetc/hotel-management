5d1872953b05f7accde28262fee7d01b3bce6978
shiyuetc <shiyuetc@users.noreply.github.com>
2020-11-03 22:04:43 +0900

    EmployeeModelを追加

---
+++ src/HotelManagement/Models/EmployeeModel.cs
@@ -0,0 +1,23 @@
+﻿using Dbflute.CBean;
+using Dbflute.ExBhv;
+using Dbflute.ExEntity;
+using Seasar.Quill.Attrs;
+using System.Linq;
+
+namespace HotelManagement.Models
+{
+    [Implementation]
+    public class EmployeeModel
+    {
+        protected MstEmployeeBhv MstEmployeeBhv;
+
+        public MstEmployee[] GetEmployeeList()
+        {
+            var mstEmployeeCB = new MstEmployeeCB();
+            mstEmployeeCB.SetupSelect_KbnRank();
+            var selectList = MstEmployeeBhv.SelectList(mstEmployeeCB);
+            var employees = selectList.Count > 0 ? selectList.ToArray() : new MstEmployee[0];
+            return employees;
+        }
+    }
+}
