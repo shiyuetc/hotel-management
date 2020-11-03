f6a4149303a35bfa85fa51678de242b54ad3784d
shiyuetc <shiyuetc@users.noreply.github.com>
2020-11-03 21:04:35 +0900

    埋め込みリソース取得クラスを追加

---
+++ src/HotelManagement/Common/ResourceUtil.cs
@@ -0,0 +1,15 @@
+﻿using HotelManagement.Enums;
+using System.Drawing;
+
+namespace HotelManagement.Common
+{
+    public static class ResourceUtil
+    {
+        public static Bitmap GetMenuIcon(Display display)
+        {
+            System.Reflection.Assembly myAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+            return new Bitmap(
+                myAssembly.GetManifestResourceStream($"HotelManagement.Resources.Images.icon_{display.ToString().ToLower()}.png"));
+        }
+    }
+}
