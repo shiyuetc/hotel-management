742136655eb61caed9ec3bc46ea3a2a8f748c57f
shiyuetc <shiyuetc@users.noreply.github.com>
2020-11-03 17:27:12 +0900

    指定したコントロールの有効/無効を一括で切り替える拡張メソッドを実装

---
+++ src/HotelManagement/Extensions/ListExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace HotelManagement.Extensions
+{
+    public static class ListExtension
+    {
+        public static void SetEnabled<T>(this List<T> list, bool value) where T : Control
+        {
+            foreach(Control control in list)
+            {
+                control.Enabled = value;
+            }
+        }
+    }
+}
