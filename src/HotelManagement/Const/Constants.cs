65d73c2091afae7689f134a3d6488caf9739eb3f
shiyuetc <shiyuetc@users.noreply.github.com>
2020-11-03 14:01:31 +0900

    Constantsに職位を追加

--- src/HotelManagement/Const/Constants.cs
+++ src/HotelManagement/Const/Constants.cs
@@ -10,5 +10,31 @@ namespace HotelManagement.Const
         /// DayOfWeek列挙体に対応した日本語
         /// </summary>
         public static char[] WeekNames = { '日', '月', '火', '水', '木', '金', '土' };
+
+        /// <summary>
+        /// 従業員の職位を表す構造体
+        /// </summary>
+        public struct Rank
+        {
+            /// <summary>
+            /// システム保守
+            /// </summary>
+            public static readonly string SystemMaintenancer = "000";
+
+            /// <summary>
+            /// アシスタントマネージャー
+            /// </summary>
+            public static readonly string AssistantManager = "001";
+
+            /// <summary>
+            /// ファイナンシャルコントローラー
+            /// </summary>
+            public static readonly string FinancialController = "002";
+
+            /// <summary>
+            /// フロントクラーク
+            /// </summary>
+            public static readonly string FrontClerk = "003";
+        }
     }
 }
