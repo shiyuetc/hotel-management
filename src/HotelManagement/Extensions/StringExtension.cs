23eae38c8b82495b89f2e77e91cf17200edac802
shiyuetc <shiyuetc@users.noreply.github.com>
2020-11-03 14:53:14 +0900

    stringの拡張クラスを追加

---
+++ src/HotelManagement/Extensions/StringExtension.cs
@@ -0,0 +1,27 @@
+﻿
+namespace HotelManagement.Extensions
+{
+    /// <summary>
+    /// stringの拡張クラス
+    /// </summary>
+    public static class StringExtension
+    {
+        /// <summary>
+        /// 文字列の値が指定された文字列群と等しいものがあるか判定します。
+        /// </summary>
+        /// <param name="value">比較する文字列</param>
+        /// <param name="parameters">含まれているか比較する文字列群</param>
+        /// <returns>等しい文字列が存在すればtrueを返す。</returns>
+        public static bool EqualsAny(this string value, params string[] parameters)
+        {
+            foreach(string parameter in parameters)
+            {
+                if(value == parameter)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
