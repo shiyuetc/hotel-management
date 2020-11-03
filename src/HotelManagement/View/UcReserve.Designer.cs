ab2d772c0747df3417fb7b051b3c57ad0e346b2c
shiyuetc <shiyuetc@users.noreply.github.com>
2020-11-03 17:02:16 +0900

    不要なコントロールを削除

--- src/HotelManagement/View/UcReserve.Designer.cs
+++ src/HotelManagement/View/UcReserve.Designer.cs
@@ -28,23 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(279, 191);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // UcReserve
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button1);
             this.Name = "UcReserve";
             this.Size = new System.Drawing.Size(929, 534);
             this.ResumeLayout(false);
@@ -52,7 +41,5 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
