116fc0f9bcce09d45587727673aba53833c25ce1
shiyuetc <shiyuetc@users.noreply.github.com>
2020-11-06 08:02:36 +0900

    TryLoginとコメントの追加

--- src/HotelManagement/Models/LoginModel.cs
+++ src/HotelManagement/Models/LoginModel.cs
@@ -6,11 +6,24 @@ using Seasar.Quill.Attrs;
 
 namespace HotelManagement.Models
 {
+    /// <summary>
+    /// ログイン関係の情報の仲介を行うクラス
+    /// </summary>
     [Implementation]
     public class LoginModel
     {
+        #region 使用するテーブル
+
         protected EmployeeBhv EmployeeBhv;
 
+        #endregion
+
+        /// <summary>
+        /// 従業員のログインを試行します。
+        /// </summary>
+        /// <param name="id">従業員番号</param>
+        /// <param name="password">パスワード</param>
+        /// <returns>ログインに成功した場合は従業員情報を返す</returns>
         public Employee Login(string id, string password)
         {
             var employeeCB = new EmployeeCB();
@@ -24,6 +37,17 @@ namespace HotelManagement.Models
             }
 
             return null;
+        }
+
+        /// <summary>
+        /// 従業員のログインが成功したかどうか判定します。
+        /// </summary>
+        /// <param name="id">従業員番号</param>
+        /// <param name="password">パスワード</param>
+        /// <returns>ログインに成功した場合はtrueを返す</returns>
+        public bool TryLogin(string id, string password)
+        {
+            return this.Login(id, password) != null;
         }
     }
 }
