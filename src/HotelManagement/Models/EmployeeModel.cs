5c8a5903db1d10c3b494d4d9fe048909456d8171
shiyuetc <shiyuetc@users.noreply.github.com>
2020-11-06 08:20:57 +0900

    EmployeeModelコメントを追記

--- src/HotelManagement/Models/EmployeeModel.cs
+++ src/HotelManagement/Models/EmployeeModel.cs
@@ -6,16 +6,29 @@ using System.Linq;
 
 namespace HotelManagement.Models
 {
+    /// <summary>
+    /// 従業員管理関係の情報の仲介を行うクラス
+    /// </summary>
     [Implementation]
     public class EmployeeModel
     {
+        #region 利用するテーブル
+
         protected EmployeeBhv EmployeeBhv;
 
+        #endregion
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>取得した従業員情報の配列を返す</returns>
         public Employee[] GetEmployeeList()
         {
             var employeeCB = new EmployeeCB();
             employeeCB.SetupSelect_Rank();
+
             var selectList = EmployeeBhv.SelectList(employeeCB);
+
             var employees = selectList.Count > 0 ? selectList.ToArray() : new Employee[0];
             return employees;
         }
