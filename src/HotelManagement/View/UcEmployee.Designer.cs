e5c83406ace91217b717dd6b01be82689165e364
shiyuetc <shiyuetc@users.noreply.github.com>
2020-11-06 12:31:44 +0900

    従業員管理画面ベース作成

--- src/HotelManagement/View/UcEmployee.Designer.cs
+++ src/HotelManagement/View/UcEmployee.Designer.cs
@@ -28,18 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.EmployeeListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
+            // 
+            // EmployeeListView
+            // 
+            this.EmployeeListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.EmployeeListView.FullRowSelect = true;
+            this.EmployeeListView.GridLines = true;
+            this.EmployeeListView.HideSelection = false;
+            this.EmployeeListView.Location = new System.Drawing.Point(3, 3);
+            this.EmployeeListView.MultiSelect = false;
+            this.EmployeeListView.Name = "EmployeeListView";
+            this.EmployeeListView.Size = new System.Drawing.Size(923, 528);
+            this.EmployeeListView.TabIndex = 2;
+            this.EmployeeListView.UseCompatibleStateImageBehavior = false;
+            this.EmployeeListView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "従業員番号";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "名前";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "職位";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "入社年月日";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "退社年月日";
             // 
             // UcEmployee
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.EmployeeListView);
             this.Name = "UcEmployee";
             this.Size = new System.Drawing.Size(929, 534);
+            this.Load += new System.EventHandler(this.UcEmployee_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.ListView EmployeeListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
     }
 }
--- src/HotelManagement/View/UcEmployee.cs
+++ src/HotelManagement/View/UcEmployee.cs
@@ -1,13 +1,48 @@
-﻿using System.Windows.Forms;
+﻿using Dbflute.ExEntity;
+using HotelManagement.Common;
+using HotelManagement.Models;
+using System.Windows.Forms;
 
 namespace HotelManagement.View
 {
+    /// <summary>
+    /// 従業員管理画面を表すフォームクラス
+    /// </summary>
     public partial class UcEmployee : UcBase
     {
+        /// <summary>
+        /// UcEmployeeクラスを初期化します。
+        /// </summary>
+        /// <param name="main"></param>
         public UcEmployee(FmMain main) : base(main)
         {
             InitializeComponent();
         }
 
+        private void UcEmployee_Load(object sender, System.EventArgs e)
+        {
+            var vm = new ModelQuillInjector<EmployeeModel>();
+            var employees = vm.Model.GetEmployeeList();
+
+            this.SetEmployeeList(employees);
+        }
+
+        private void SetEmployeeList(Employee[] employees)
+        {
+            this.EmployeeListView.Items.Clear();
+            ListViewItem[] listViewItems = new ListViewItem[employees.Length];
+            for (int i = 0; i < employees.Length; i++)
+            {
+                listViewItems[i] = new ListViewItem(new string[] {
+                    employees[i].EmployeeNo,
+                    $"{employees[i].LastName} {employees[i].FirstName}",
+                    employees[i].Rank.JapaneseName,
+                    employees[i].EntryDate.Value.ToShortDateString(),
+                    employees[i].IsLeave ? employees[i].LeaveDate.Value.ToShortDateString() : string.Empty
+                });
+            }
+            this.EmployeeListView.Items.AddRange(listViewItems);
+            this.EmployeeListView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
     }
 }
