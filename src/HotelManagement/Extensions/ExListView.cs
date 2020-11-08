e9dbab2b4ef1ef2fd5b8137e82c05c76f9a54dac
shiyuetc <shiyuetc@users.noreply.github.com>
2020-11-08 09:45:39 +0900

    ExListViewを追加

---
+++ src/HotelManagement/Extensions/ExListView.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+
+namespace HotelManagement.Extensions
+{
+    public class ExListView : ListView
+    {
+        public void Init(ListViewItem[] listViewItems)
+        {
+            this.Visible = false;
+            this.Items.Clear();
+            this.Items.AddRange(listViewItems);
+            this.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            this.Visible = true;
+        }
+
+        public void UpdateItem(int rowIndex, ListViewItem listViewItem)
+        {
+            this.Items[rowIndex] = listViewItem;
+            this.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+    }
+}
