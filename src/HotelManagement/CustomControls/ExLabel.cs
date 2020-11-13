7c04e13a909e40f4d78be65976ca3f5e690ad868
shiyuetc <shiyuetc@users.noreply.github.com>
2020-11-13 20:37:04 +0900

    ExLabelを追加

---
+++ src/HotelManagement/CustomControls/ExLabel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HotelManagement.CustomControls
+{
+    public class ExLabel : Label
+    {
+        private readonly Color EnableForeColor = Color.FromArgb(255, 255, 255);
+
+        private readonly Color EnableBackColor = Color.FromArgb(73, 111, 225);
+
+        private readonly Color DisableForeColor = Color.FromArgb(255, 255, 255);
+
+        private readonly Color DisableBackColor = Color.FromArgb(120, 120, 120);
+
+        public ExLabel()
+        {
+            this.Size = new Size(100, 19);
+            this.BorderStyle = BorderStyle.Fixed3D;
+            this.TextAlign = ContentAlignment.MiddleRight;
+            this.SetEnable(true);
+        }
+
+        public void SetEnable(bool value)
+        {
+            if(value)
+            {
+                this.ForeColor = this.EnableForeColor;
+                this.BackColor = this.EnableBackColor;
+            }
+            else
+            {
+                this.ForeColor = this.DisableForeColor;
+                this.BackColor = this.DisableBackColor;
+            }
+        }
+    }
+}
