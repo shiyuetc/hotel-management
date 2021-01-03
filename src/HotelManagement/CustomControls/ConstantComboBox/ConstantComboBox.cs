9fd72acb67d4f10ace57d9f51dbe75929ec0cec5
shiyuetc <shiyuetc@users.noreply.github.com>
2021-01-03 13:06:14 +0900

    区分値用のコンボボックスを追加

---
+++ src/HotelManagement/CustomControls/ConstantComboBox/ConstantComboBox.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace HotelManagement.CustomControls.ConstantComboBox
+{
+    /// <summary>
+    /// 区分値用のコンボボックスを表すクラス
+    /// </summary>
+    public abstract class ConstantComboBox : ComboBox
+    {
+        /// <summary>
+        /// ValueMemberの値を参照するための配列
+        /// </summary>
+        protected string[] Values { get; private set; }
+
+        /// <summary>
+        /// ConstantComboBoxクラスを初期化します。
+        /// </summary>
+        public ConstantComboBox()
+        {
+            this.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        /// <summary>
+        /// DataSourceとMemberを設定します。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array">DataSource</param>
+        /// <param name="displayMember">DisplayMember</param>
+        /// <param name="valueMember">ValueMember</param>
+        protected void SetMember<T>(T[] array, string displayMember, string valueMember)
+        {
+            this.DisplayMember = displayMember;
+            this.ValueMember = valueMember;
+            this.DataSource = array;
+
+            // ValueMemberの値を変数に記憶
+            var valueMemberProperty = typeof(T).GetProperty(valueMember);
+            this.Values = new string[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                this.Values[i] = valueMemberProperty.GetValue(array[i]).ToString();
+            }
+        }
+
+        /// <summary>
+        /// ValueMemberの値を指定して対象の項目を選択します。
+        /// </summary>
+        /// <param name="value">ValueMemberの値</param>
+        public void SelectFromValue(string value)
+        {
+            this.SelectedIndex = Array.IndexOf(this.Values, value);
+        }
+
+    }
+}
---
+++ src/HotelManagement/CustomControls/ConstantComboBox/RankComboBox.cs
@@ -0,0 +1,18 @@
+﻿using Dbflute.AllCommon;
+
+namespace HotelManagement.CustomControls.ConstantComboBox
+{
+    /// <summary>
+    /// 職位区分のコンボボックスを表すクラス
+    /// </summary>
+    public class RankComboBox : ConstantComboBox
+    {
+        /// <summary>
+        /// RankComboBoxクラスを初期化します。
+        /// </summary>
+        public RankComboBox()
+        {
+            this.SetMember(CDef.Rank.Values, "Alias", "Code");
+        }
+    }
+}
